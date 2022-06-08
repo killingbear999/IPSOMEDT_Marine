411cef403fb97dde58fc6d42e67d9894fa56405d
LAPTOP-6DPEQ9IL\ZihaoWang <e0424629@u.nus.edu>
2022-06-08 13:33:16 +0800

    update

---
+++ AppsThatShareDataExample/AppsThatShareDataExample.m
@@ -0,0 +1,10 @@
+%% Example: Plotting App That Opens a Dialog Box
+% This app consists of a main plotting app that has a button
+% for selecting options in a dialog box. The *Options* button
+% calls the dialog box app with input arguments. In the dialog box, 
+% the callback for the *OK* button sends the user's selections back to the main
+% app by calling a public function in the main app. 
+%
+% <<../multiwindowapp_screenshot.png>>
+
+% Copyright 2017 The MathWorks, Inc.
